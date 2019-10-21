b8b407f6e08770de6bf518550324f5f87680d431
Takeshi Katano <takatano@outlook.com>
2019-10-22 00:26:16 +0900

    Split the class file

---
+++ src/NativeHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.InteropServices;
+
+namespace BgImgUsingWinSpotlight
+{
+    internal class NativeHelper
+    {
+        internal static class NativeMethods
+        {
+            public const uint SPI_SETDESKWALLPAPER = 0x0014;
+            public const uint SPIF_UPDATEINIFILE = 0x01;
+            public const uint SPIF_SENDWININICHANGE = 0x02;
+
+            [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool SystemParametersInfo(uint uiAction, uint uiParam, string pvParam, uint fWinIni);
+        }
+
+        public static bool ChangeDesktopBackgroundImage(string desktopBackgroundIamgeFilePath)
+        {
+            return NativeMethods.SystemParametersInfo(NativeMethods.SPI_SETDESKWALLPAPER, 0, desktopBackgroundIamgeFilePath, NativeMethods.SPIF_UPDATEINIFILE | NativeMethods.SPIF_SENDWININICHANGE);
+        }
+    }
+}
